b018cdf0e5b428483ae0175fd2e1a91fd34086a0
Manu Arora <mannuarora700@gmail.com>
2018-12-22 20:54:52 +0530

    Better approach -- multiple pointers

--- sumZero.js
+++ sumZero.js
@@ -1,7 +1,7 @@
 // write a function called sumZero which accepts a sorted array of integers.
 // The function should find the first pair where the sum is 0.
 // Return an array that includes both values that sum to zero or undefined if a pair does not exist.
-
+// Naive approach
 function sumZero(arr) {
 	for(let i = 0; i < arr.length; i++) {
 		for(let j = i+1; j <arr.length; j++) {
@@ -15,4 +15,4 @@ function sumZero(arr) {
 // Time complexity -- O(n^2)
 // Space complexity -- O(1)
 
-console.log(sumZero([-3, -2, -1, 0, 1, 2, 3]))
+console.log(sumZero([-4, -3, -2, -1, 0, 1, 2, 5]))
