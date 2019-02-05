16dc6082ac6635b72e03606e692b28967d8af2f8
Manu Arora <mannuarora700@gmail.com>
2019-02-05 19:29:14 +0530

    Sliding window for maxSubarraySum problem

---
+++ maxSubarraysum-slidingWindow.js
@@ -0,0 +1,18 @@
+function maxSubarraysum(arr, num) {
+	let maxSum = 0;
+	let tempSum = 0;
+	if(arr.length < num) return null;
+
+	for(let i = 0; i < num; i++) {
+		maxSum += arr[i];
+	}
+	tempSum = maxSum;
+
+	for(let i = num; i < arr.length; i++) {
+		tempSum = tempSum - arr[i-num] + arr[i];
+		maxSum = Math.max(maxSum, tempSum);
+	}
+	console.log("Maximum sum is :", maxSum);
+	return maxSum;
+}
+maxSubarraysum([1,3,7,2,3,4,99,53,234,64], 3);
